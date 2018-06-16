5e584c4007e05d2cc19518a6a9ee5ee17982fdca
xianghao <xianghao1991@gmail.com>
2018-06-16 13:22:02 +0800

    add ip and host to ALLOWED_HOSTS

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'qq(z$f2v0-mg)c8qpcj=)0=))71n!k+8*m%jn70os4zhjmwfs7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.smoba.cn', '193.112.164.104']
 
 # Application definition
 
@@ -74,18 +74,18 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
-        'NAME': 'blogproject',  # 你要存储数据的库名，事先要创建之
-        'USER': 'root',  # 数据库用户名
-        'PASSWORD': 'root',  # 密码
-        'HOST': 'localhost',  # 主机
-        'PORT': '3306',  # 数据库使用的端口
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+    #     'NAME': 'blogproject',  # 你要存储数据的库名，事先要创建之
+    #     'USER': 'root',  # 数据库用户名
+    #     'PASSWORD': 'root',  # 密码
+    #     'HOST': 'localhost',  # 主机
+    #     'PORT': '3306',  # 数据库使用的端口
+    # }
 }
 
 # Password validation
