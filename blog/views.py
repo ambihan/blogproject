2a409386bc8d84f6def9bd4e41769d37532d433b
xianghao <xianghao1991@gmail.com>
2018-07-21 11:55:46 +0800

    1.统计阅读量 2.自动生成摘要 3.正确显示评论数、阅读数

--- blog/views.py
+++ blog/views.py
@@ -25,6 +25,9 @@ def category(request, pk):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+
+    post.increase_views()
+
     post.body = markdown.markdown(post.body,
                                   extensions=[
                                       'markdown.extensions.extra',
